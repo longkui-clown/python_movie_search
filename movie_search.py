534af285ca6d0427b652fcd75df30a55804ac595
long <315202350@qq.com>
2019-02-16 12:33:21 +0800

    优化

--- movie_search.py
+++ movie_search.py
@@ -67,15 +67,29 @@ def first_search(movie_name):
                 res = re.findall('<span.*?><a href=(.*?) target=_blank>(.*?)</a>', str_table)
                 res_num = len(res)
                 threads = {}
+                t_res = []
+                max_len = 0
+
                 for (k, v) in res:
                     n_f_v = re.findall('.*>(.*?)<.*', v)
                     if n_f_v != []:
                         v = n_f_v[0]
+                    t_res.append((k, v))
+                    t_len = len(bytes(v,encoding='gb18030'))
+                    if t_len > max_len:
+                        max_len = t_len
+                    
+                idx = 0
+                print('%s' % ("".ljust(75 + max_len, "-")))
+                for (k, v) in t_res:
                     # find_movies[(k, v)] = second_search(k)
+                    print('|   %-5d %s %s' % (idx + 1, k.ljust(60, " "), v.ljust(max_len, " ")))
+                    idx += 1
                     t = threading.Thread(target=second_search,args=(find_movies, (k, v), k))
                     t.setDaemon(True)
                     t.start()
                     threads[(k, v)] = t
+                print('%s' % ("".ljust(75 + max_len, "-")))
 
                 for k, v in threads.items():
                     v.join()
@@ -91,6 +105,13 @@ def first_search(movie_name):
     }
 
 
+# def get_space_add(v):
+#     space_addition = 0
+#     for i in v:
+#         if len(bytes(i,encoding='gb18030')) == 1:
+#             space_addition += 1
+#     return space_addition
+
 def search_movie(movie_name):
     res = first_search(movie_name)
     print("共查找到 " + str(res["num"]) + "个结果：")
@@ -104,12 +125,6 @@ def search_movie(movie_name):
 
 def generate_res(find_movies):
     ks = list(find_movies.keys())
-    idx = 0
-    print('---------------------------------------------------------------------------------------------')
-    for (m_url, m_title) in ks:
-        print('| %d %s %s |' % (idx + 1, m_title.ljust(45, " "), m_url.ljust(45, " ")))
-        idx += 1
-    print('---------------------------------------------------------------------------------------------')
     while 1:
         try:
             choose_index = input("\n请输入选择的序号（1 - %d）(输入 q 退出)：\n" % (len(ks)))
@@ -119,30 +134,31 @@ def generate_res(find_movies):
 
             elif (eval(choose_index) <= len(ks)) and (eval(choose_index) > 0):
                 print(format_links(ks[eval(choose_index) - 1], find_movies[ks[eval(choose_index) - 1]]))
-                print("格式化完成")
+                print("\n*********** 格式化完成 ***********")
         except:
             ""
 
 
 format_order = ['online_watch', 'baiduyun', 'cili']
+init_left_str = '--'
 def format_links(src, links):
-    (f_n, f_s_url) = src
-    formated = f_n + "    " + f_s_url + '\n'
+    (f_s_url, f_n) = src
+    formated = "*********** " + f_n + " " + f_s_url + " ***********\n"
     for ooo in format_order:
         if ooo in links.keys():
             formated += per_format(ooo, links[ooo]) + '\n'
         else:
             formated += ""
     
-    return formated.replace('\n\n', '').strip('\n').strip(' ')
+    return formated.replace(init_left_str + init_left_str, init_left_str).replace(init_left_str + '\n', '').replace('\n\n', '').strip('\n').strip(' ')
 
 
 def per_format(type, links):
-    res = ""
+    res = init_left_str
     for link in links:
         if type == 'cili':
             (ed2k, film_name) = link
-            res += film_name + "    " + ed2k + '\n'
+            res += init_left_str + film_name + "\n  " + ed2k + '\r\n\r\n'
         elif type in format_order:
             res += " ".join(link).strip(" ")
         else:
@@ -157,6 +173,7 @@ def main():
         while 1:
             movie_name = input("请输入查找的电影名字: \n")
             # print(movie_name)
+            # movie_name = "胡桃夹子"
             search_movie(movie_name)
     except:
         print("退出查找")
